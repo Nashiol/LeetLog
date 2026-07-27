26748d7766ebe849e896f486f4241803604e4fc1
Nashiol Nyirongo <nyirongonashiol@gmail.com>
2026-07-27 17:53:19 +0200

    UPDATE: Chaged database to local postgres.

--- leetlog/settings.py
+++ leetlog/settings.py
@@ -81,14 +81,14 @@ WSGI_APPLICATION = 'leetlog.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("SUPABASE_DB_NAME"),
-        "USER": config("SUPABASE_DB_USER"),
-        "PASSWORD": config("SUPABASE_DB_PASSWORD"),
-        "HOST": config("SUPABASE_DB_HOST"),
-        "PORT": config("SUPABASE_DB_PORT", cast=int, default=5432),
-        "OPTIONS": {
-            "sslmode": "require",
-        },
+        "NAME": config("POSTGRES_DB_NAME"),
+        "USER": config("POSTGRES_DB_USER"),
+        "PASSWORD": config("POSTGRES_DB_PASSWORD"),
+        "HOST": config("POSTGRES_DB_HOST"),
+        "PORT": config("POSTGRES_DB_PORT", cast=int, default=5432),
+        # "OPTIONS": {
+        #     "sslmode": "require",
+        # },
     }
 }
 
